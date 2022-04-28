f8c4c02e2ca7ab2105cb91e936aaa7c81bb0e26e
SantaKrauze <gracjantokarz38@gmail.com>
2022-04-28 21:03:51 +0200

    didnt work, fixed indexing in for loop

--- randStrMerge.py
+++ randStrMerge.py
@@ -3,10 +3,12 @@
 import math
 import sys
 import os
+import time
 import numpy as np
 
-path1 = "qrng/QNGFile5.dat"
-path2 = "qrng/QNGFile6.dat"
+t1 = time.time()
+path1 = "qrng/QNGFile1.dat"
+path2 = "qrng/QNGFile2.dat"
 shift = int(sys.argv[1])
 
 pathOut = sys.argv[2]
@@ -16,15 +18,16 @@ bin1 = np.unpackbits(file1)
 file2 = np.fromfile(path2,dtype='uint8')
 bin2 = np.unpackbits(file2)
 
-size = len(bin1) 
+size = len(bin1)
 bin3 = bin1^bin2
 binMerged = np.empty(3*size,dtype=int)
 
 print("=============================")
 print('File 1:',path1,'\nFile 2:',path2,'\nOutput:',pathOut,'\nShift =',shift)
 
+n = 0
 for i in range(0, math.floor(size/shift)):
-    for j in range(0,2):
+    for j in range(0,3):
         for k in range(i*shift, (i+1)*shift):
             if j == 0:
                 x=bin1[k]
@@ -32,11 +35,20 @@ for i in range(0, math.floor(size/shift)):
                 x=bin2[k]
             else:
                 x=bin3[k]
-            binMerged[k] = x
-        print("\r",f'Merging: {i/(size/shift)*100:.1f}%',end="")
+            binMerged[n] = x
+            #print(n)
+            n +=1
+            #print(k+j*shift,bin1[k],bin2[k],bin3[k],x)
+            print("\r",f'Merging: {n/3/size*100:.2f}%',end="")
 
 out = np.packbits(binMerged)
-total = len(out)
-print('\nDone. \nTotal size: ',len(out),'B')
+total = n
+#print(binMerged)
+#print(out)
+t2 = time.time()
+sec = t2 - t1
+mins = math.floor(sec/60)
+
+print('\nDone in {0:.0f} min {1:.0f} s. \nTotal size: {2}b'.format(mins,sec%60,total))
 print("=============================\n")
 out.tofile(pathOut,sep="")
